c102b57666a34002aa88e1eef5a255ba9175b939
yashwanth s <yahwanthyadav0106@gmail.com>
2026-03-18 10:31:11 +0530

    UC3:commandline-arguments

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,7 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Get the name from command-line argument
-        String name = args[0];
+        // Default name
+        String name = "World";
+
+        // Check if a name is provided as a command-line argument
+        if (args.length > 0) {
+            name = args[0]; // Use the provided name
+        }
+
         System.out.println("Hello, " + name + "!");
     }
 }
