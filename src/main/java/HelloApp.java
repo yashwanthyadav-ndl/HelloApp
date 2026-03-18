8f25ca0a5188ba02229408c3bfb3e829eaa0b862
yashwanth s <yahwanthyadav0106@gmail.com>
2026-03-18 10:22:05 +0530

    UC2-using-commandline

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,2 +1,7 @@
 public class HelloApp {
+    public static void main(String[] args) {
+        // Get the name from command-line argument
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+    }
 }
